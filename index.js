7cf88293fcbdff15d3391d0cc66e01af3d7f6225
Ahmad Mash <wolfmashagbah@hotmail.com>
2023-09-28 13:01:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+    function writeCards(names) {
+    const thankYou = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYou.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYou;
+  }
+  
+  function countDown(x) {
+    while (x >= 0) {
+      console.log(x);
+      x--;
+    }
+  }
+  
+  const writeCardsResult = writeCards(['Guadalupe', 'Ollie', 'Aki']);
+  console.log(writeCardsResult);
+  
+  countDown(10);
+  
